bc8fc5e648c90e9770584d68f0fc8f95d59a1805
Rostyslav_Stakhiv <rostyk96@gmail.com>
2018-02-06 16:45:39 +0200

    - Initialized Cross-Platform sorting framework

---
+++ Source/SortingAlgorithms.h
@@ -0,0 +1,16 @@
+//
+//  SortingAlgorithms.h
+//  SortingAlgorithms
+//
+//  Created by admin on 2/5/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SortingAlgorithms.
+FOUNDATION_EXPORT double SortingAlgorithms_iOSVersionNumber;
+
+//! Project version string for SortingAlgorithms.
+FOUNDATION_EXPORT const unsigned char SortingAlgorithms_VersionString[];
+
+

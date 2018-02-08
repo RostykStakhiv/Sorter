cc4fa37c231011c3d7b152bcdad80eaa34bd78f7
Rostyslav_Stakhiv <rostyk96@gmail.com>
2018-02-08 18:17:46 +0200

    - Recreated project for one single target

---
+++ Sorter/Sorter/Sorter.h
@@ -0,0 +1,19 @@
+//
+//  Sorter.h
+//  Sorter
+//
+//  Created by admin on 2/8/18.
+//  Copyright © 2018 rstakhiv. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Sorter.
+FOUNDATION_EXPORT double SorterVersionNumber;
+
+//! Project version string for Sorter.
+FOUNDATION_EXPORT const unsigned char SorterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sorter/PublicHeader.h>
+
+
